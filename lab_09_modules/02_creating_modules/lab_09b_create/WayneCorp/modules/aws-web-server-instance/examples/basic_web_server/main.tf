21723fff0ef4d953246ce1ac11cef53bcd7569fb
Rafa <rafa@portillo.eu>
2023-03-27 14:45:48 +0200

    fixes in module tests

--- lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/aws-web-server-instance/examples/basic_web_server/main.tf
+++ lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/aws-web-server-instance/examples/basic_web_server/main.tf
@@ -2,6 +2,8 @@
 ## Create an auxiliary VPC
 module "vpc_test" {
   source            = "../../../../modules/aws-web-vpc"
+  vpc_name = "test_2"
+  subnet_name = "sub_2"
   vpc_cidr_block    = "10.99.0.0/16"
   subnet_cidr_block = "10.99.100.0/24"
 }
@@ -15,5 +17,5 @@ module "web_test" {
 
   vpc_id        = module.vpc_test.vpc_id
   subnet_id     = module.vpc_test.subnet_id
-  sec_group_ids = [module.vpc_test.sec_group_id]
+  sec_group_ids = [module.vpc_test.web_sec_group_id]
 }
