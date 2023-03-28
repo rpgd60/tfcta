cc6a44331097dd0f01c680bc01b2cdce540c5c9e
Rafa <rafa@portillo.eu>
2023-03-28 17:00:08 +0200

    bucket module

--- lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/s3_bucket/main.tf
+++ lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/s3_bucket/main.tf
@@ -3,19 +3,17 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.bucket_name}"
 
-
 }
 
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.my_bucket.id
   versioning_configuration {
-    status = var.versioing ? "Enabled" : "Disabled"
+    status = var.versioning ? "Enabled" : "Disabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+  bucket = aws_s3_bucket.my_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -26,22 +24,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
 
 resource "aws_s3_bucket_public_access_block" "block_public" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+  bucket                  = aws_s3_bucket.my_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-## DynamoDB table for locking state in S3
-resource "aws_dynamodb_table" "terraform_state_locks" {
-  name         = "${var.project}-terraform-state-locks-${var.environment}"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
--- lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/s3_bucket/variables.tf
+++ lab_09_modules/02_creating_modules/lab_09b_create/WayneCorp/modules/s3_bucket/variables.tf
@@ -1,10 +1,13 @@
+# Required variable
 variable bucket_name {
   type = string
   description = "Bucket name - must be globally unique"
 }
+
+## Variable is optional 
 variable "versioning" {
   description = "Enable versioning."
-  type        = boolean
+  type        = bool
   default     = false
 }
 
